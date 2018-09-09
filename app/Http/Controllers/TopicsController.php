481cddcd57603cc57ebcf960b326ce0add19ff47
yaojinhui <yaojinhui1993@gmail.com>
2018-09-09 09:41:55 +0800

    Add topic detail page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -37,7 +37,7 @@ class TopicsController extends Controller
     {
         $topic = Auth::user()->topics()->create($request->all());
         // $topic = Topic::create($request->all());
-        return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+        return redirect()->route('topics.show', $topic->id)->with('message', '成功创建主题！');
     }
 
     public function edit(Topic $topic)
@@ -51,7 +51,7 @@ class TopicsController extends Controller
         $this->authorize('update', $topic);
         $topic->update($request->all());
 
-        return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+        return redirect()->route('topics.show', $topic->id)->with('message', '更新成功！');
     }
 
     public function destroy(Topic $topic)
@@ -59,7 +59,7 @@ class TopicsController extends Controller
         $this->authorize('destroy', $topic);
         $topic->delete();
 
-        return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+        return redirect()->route('topics.index')->with('message', '成功删除！');
     }
 
     public function uploadImage(Request $request, ImageUpload $handler)
