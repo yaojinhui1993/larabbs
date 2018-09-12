1dc132175c313606f5899617e1b3c19373bd7661
yaojinhui <yaojinhui1993@gmail.com>
2018-09-12 13:55:46 +0800

    topic delete

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -26,4 +26,13 @@ class TopicsController extends Controller
 
         return $this->response->item($topic, new TopicTransformer());
     }
+
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy', $topic);
+
+        $topic->delete();
+
+        return $this->response->noContent();
+    }
 }
